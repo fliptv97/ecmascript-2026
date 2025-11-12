9d0aad12fec56c7cd6f4908624c5bd4a40a4b487
Philip Kazakov <fliptv97@gmail.com>
2025-11-12 12:39:03 +0200

    remove unnecessary constructor from environment record class

--- src/environment-records/environment-record.js
+++ src/environment-records/environment-record.js
@@ -1,9 +1,5 @@
 // https://tc39.es/ecma262/multipage/executable-code-and-execution-contexts.html#sec-environment-records
 export class EnvironmentRecord {
-  constructor(outerEnv = null) {
-    this.outerEnv = outerEnv;
-  }
-
   hasBinding(_n) {}
 
   createMutableBinding(_n, _d) {}
