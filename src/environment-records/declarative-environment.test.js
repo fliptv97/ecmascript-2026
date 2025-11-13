40a7e92258d23c2adc6843a858c4fcab846ad1cc
Philip Kazakov <fliptv97@gmail.com>
2025-11-13 16:33:01 +0200

    improve imports clarity in declarative-environment.test.js

--- src/environment-records/declarative-environment.test.js
+++ src/environment-records/declarative-environment.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
 import { Flags as BindingFlags } from "../binding.js";
-import { Completion, Type } from "../completion.js";
+import { Completion, Type as CompletionType } from "../completion.js";
 import { UNUSED } from "../constants.js";
 import { hasFlag } from "../utils.js";
 import { DeclarativeEnvironment } from "./declarative-environment.js";
@@ -14,7 +14,7 @@ describe("DeclarativeEnvironment", () => {
     const opResult = envRec.createMutableBinding(ident, false);
 
     assert(opResult instanceof Completion);
-    assert(opResult.type == Type.NORMAL);
+    assert(opResult.type == CompletionType.NORMAL);
     assert(opResult.value === UNUSED);
 
     assert(envRec._bindings.size === 1);
