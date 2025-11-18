dc532055a6fd4d851cbc9b2a2e27970b0b4a41e0
Philip Kazakov <fliptv97@gmail.com>
2025-11-18 15:47:11 +0200

    add `isDataDescriptor` util

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,11 @@
 export function hasFlag(bitmask, mask) {
   return (bitmask & mask) === mask;
 }
+
+// https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html#sec-isdatadescriptor
+export function IsDataDescriptor(desc) {
+  if (Object.hasOwn(desc, "value")) return true;
+  if (Object.hasOwn(desc, "writable")) return true;
+
+  return false;
+}
